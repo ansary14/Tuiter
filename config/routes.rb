bc056fa6e89afa57c28c05f67908e7c5ceb36fce
Md. Ayub Ansary <112767766+ansary14@users.noreply.github.com>
2023-03-01 18:37:31 +0600

    Revert "Room"

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  resources :rooms do
-    resources :messages
-  end
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'pages#home'
-  get 'user/:id', to: 'users#show', as: 'user'
+  # root "articles#index"
 end
